0a53209347cc4b891e73c4e44f818ea30d0b9c27
Karen K <karentutor1@gmail.com>
2021-04-17 09:07:15 -0700

    Dabase tables added

--- sql.sql
+++ sql.sql
@@ -1,8 +1,32 @@
-CREATE TABLE users (
-id INT(20),
-name VARCHAR(30) NOT NULL,
-email VARCHAR(30) NOT NULL,
-password VARCHAR(50) NOT NULL,
-date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-role VARCHAR(30) NOT NULL
-);
+CREATE TABLE `patients` (
+  `_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `information` varchar(150) NOT NULL,
+  `postedBy` varchar(50) NOT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `records` (
+  `_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `patient_id` int unsigned NOT NULL,
+  `title` varchar(30) NOT NULL,
+  `body` varchar(150) NOT NULL,
+  `postedBy` varchar(50) NOT NULL,
+  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` varchar(30) NOT NULL,
+  PRIMARY KEY (`_id`)
+) ENGINE=InnoDB;
+
+CREATE TABLE `users` (
+  `_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` char(128) NOT NULL,
+  `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` varchar(30) NOT NULL DEFAULT 'subscriber',
+  PRIMARY KEY (`_id`)
+) ENGINE=InnoDB;
+
